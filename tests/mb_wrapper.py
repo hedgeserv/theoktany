5350d4b3d5e535ac31431188b2dfa1209df87c0b
shair <shair@hedgeserv.com>
2015-10-27 12:38:57 -0400

    Added missing files lost during branch

---
+++ tests/mb_wrapper.py
@@ -0,0 +1,82 @@
+import json
+import os
+import subprocess
+from subprocess import Popen
+
+import requests
+
+
+# Note: changing these will NOT change how the mountebank process starts.
+PROTOCOL = 'http'
+MOUNTEBANK_HOST = PROTOCOL + '://localhost'
+MOUNTEBANK_URL = MOUNTEBANK_HOST + ':2525'
+IMPOSTERS_URL = MOUNTEBANK_URL + '/imposters'
+
+
+class MountebankProcess(object):
+    def __init__(self):
+        self.mb_proc = None
+        self.imposters = []
+
+    @property
+    def url(self):
+        return MOUNTEBANK_URL
+
+    def get_imposter_url(self, port):
+        return '{}:{}'.format(MOUNTEBANK_HOST, port)
+
+    def start(self):
+        # TODO: shair: mb is not starting correctly
+        self.mb_proc = Popen(
+            'mb', shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=None)
+        if self.mb_proc.poll() is not None:
+            raise Exception('Mountebank did not start properly.')
+
+    def create_imposter(self, stub_filename, port=5555):
+        imposter = self.create_stubs(stub_filename, port=port)
+        response = requests.post(IMPOSTERS_URL, json=imposter)
+        if response.status_code != 201:
+            print(response.content)
+            raise Exception('Could not load Mountebank imposter.')
+        port = response.json()['port']
+        self.imposters.append(port)
+
+        return port
+
+    def destroy_imposter(self, port):
+        try:
+            self.imposters.remove(port)
+        except ValueError:
+            pass
+        return requests.delete("{}/imposters/:{}".format(MOUNTEBANK_HOST, port))
+
+    def destroy_all_imposters(self):
+        return requests.delete(IMPOSTERS_URL, verify=False)
+
+    def stop(self):
+        self.mb_proc.terminate()
+        try:
+            return_code = self.mb_proc.wait(timeout=5)
+        except TimeoutError:
+            raise Exception('Mountebank did not stop properly.')
+        if return_code != 0:
+            print('Mountebank closed with a status of {}.'.format(return_code))
+
+    def create_stubs(self, stub_filename, port=5555, name='imposter'):
+        # shamelessly stolen from Tim and grid-webapp-client, then slightly modified somewhat modified
+        absolute_path = os.path.join(os.path.dirname(__file__), stub_filename)
+        with open(absolute_path, 'r') as stub_file:
+            stubs = json.load(stub_file)
+
+        # We store the body as JSON in the files for legibility but Mountebank wants the body as a string.
+        for stub in stubs:
+            for response in stub['responses']:
+                response['is']['body'] = json.dumps(response['is']['body'])
+
+        # impostor = {"port": 5555, "protocol": "http", "name": "TCK", "stubs": stubs}
+        return {"port": port, "protocol": PROTOCOL, "name": name, "stubs": stubs}
+
+        # response = requests.post(url='http://localhost:2525/imposters',
+        #                          json=impostor,
+        #                          headers={'Accept': 'application/json', 'Content-Type': 'application/json'})
+        # assert response.status_code == 201, 'Error creating mb stub {}'.format(response.status_code)
---
+++ theocktany/exceptions.py
@@ -0,0 +1,10 @@
+class ApiException(Exception):
+    pass
+
+
+class SerializerException(Exception):
+    pass
+
+
+class EnrollmentException(ApiException):
+    pass
